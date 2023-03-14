320666b9935ec7248cd886f0ce9a571591071bc5
Akira Pinemoon <paulik080705@gmail.com>
2023-03-14 12:02:02 +0000

    setup tauri & vue started programming home view started linking to backend

---
+++ src/App.vue
@@ -0,0 +1,9 @@
+<script setup>
+import Home from "./views/Home.vue";
+</script>
+
+<template>
+  <div class="w-full h-screen flex flex-col place-items-center justify-around bg-gray-900 text-white">
+    <Home />
+  </div>
+</template>
---
+++ src/views/Home.vue
@@ -0,0 +1,30 @@
+<script setup>
+import { ref } from "vue";
+import { invoke } from "@tauri-apps/api/tauri";
+import { open } from "@tauri-apps/api/dialog"
+
+const selectedDir = ref("Choose Directory");
+
+async function processDirectory() {
+  if (selectedDir.value == "") return;
+  if (selectedDir.value == "Choose Directory") return;
+  alert(await invoke("process_directory", { directory: selectedDir.value }));
+}
+
+async function pickDirectory() {
+  const newDir = await open({ directory: true });
+  if (newDir == null) return;
+  selectedDir.value = newDir;
+}
+</script>
+
+<template>
+  <div class=" bg-gray-700 w-[50vh] max-w-full h-[40vh] flex flex-col place-items-center justify-center gap-4 rounded">
+    <h1 class=" text-xl">UmlBot 🤖</h1>
+    <button @click="pickDirectory" class="p-3 truncate overflow-hidden bg-gray-500 w-5/6 h-[10vh] rounded">{{ selectedDir }}</button>
+    <button @click="processDirectory" class="p-3 bg-green-500 hover:bg-green-700 w-5/6 h-[10vh] rounded flex flex-row place-items-center justify-between">
+      Process
+      <img src="../assets/arrow-circle-right.svg" class=" h-full" />
+    </button>
+  </div>
+</template>
